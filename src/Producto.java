165549bd33bf5c20a67ee7dfe68ad6b3c6078eb8
camilaFernandez <maria.fernandezo1@correo.usa.edu.co>
2020-08-25 20:18:03 -0500

    Creacion de clase Producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,3 +1,13 @@
 public class Producto {
-    
+    TarjetaCredito tarjetaCredito;
+    TarjetaDebito tarjetaDebito;
+    Cuenta cuenta;
+    Prestamo presta;
+
+    public Producto(TarjetaCredito tarjetaCredito, TarjetaDebito tarjetaDebito, Cuenta cuenta, Prestamo presta) {
+        this.tarjetaCredito = tarjetaCredito;
+        this.tarjetaDebito = tarjetaDebito;
+        this.cuenta = cuenta;
+        this.presta = presta;
+    }
 }
